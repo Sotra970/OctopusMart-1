57d6c1cd15b07d518e164c41d0a39b81df221bab
Ahmed Naeem <a7mn3e@gmail.com>
2018-01-18 00:01:05 +0200

    naeem ver1

---
+++ app/src/main/java/com/example/ahmed/octopusmart/BadgeConfig/Utils.java
@@ -0,0 +1,35 @@
+package com.example.ahmed.octopusmart.BadgeConfig;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
+
+import com.example.ahmed.octopusmart.R;
+
+/**
+ * Created by ahmed on 12/4/2017.
+ */
+
+public class Utils {
+    public static void setBadgeCount(Context context, LayerDrawable icon, int count) {
+
+        BadgeDrawable badge;
+
+        // Reuse drawable if possible
+        Drawable reuse = icon.findDrawableByLayerId(R.id.ic_badge);
+        if (reuse != null && reuse instanceof BadgeDrawable) {
+            badge = (BadgeDrawable) reuse;
+        } else {
+            badge = new BadgeDrawable(context);
+        }
+
+        badge.setCount(count);
+        icon.mutate();
+        icon.setDrawableByLayerId(R.id.ic_badge, badge);
+    }
+
+    public static boolean isLollipopOrhigher() {
+        return Build.VERSION.SDK_INT >= 21;
+    }
+}
